0026692186d6b8d14df30389dd5cc981c6c09371
hetunccc <111727859+hetunccc@users.noreply.github.com>
2023-02-11 10:18:45 +0900

    studentsystem

---
+++ src/com/itheima/studentsystem/StudentSystem.java
@@ -0,0 +1,20 @@
+package com.itheima.studentsystem;
+
+import java.util.Scanner;
+
+public class StudentSystem {
+    public static void main(String[] args) {
+        System.out.println("----------------welcom to heima studentsystem---------------");
+        System.out.println("1:add student");
+        System.out.println("2:delete student");
+        System.out.println("3:remove student");
+        System.out.println("4:search student");
+        System.out.println("5:exit");
+        System.out.println("put in your choose");
+        Scanner sc = new Scanner(System.in);
+        String choose = sc.next();
+        switch (choose){
+
+        }
+    }
+}
